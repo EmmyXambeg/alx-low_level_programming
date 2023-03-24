cc19909075a6878ab6f9f4703d45e14642efc133
EmmyXambeg <xamsinnovation@gmail.com>
2023-03-23 21:44:12 -0700

    Task 3 updated 0x0F. C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: 0
 */
-int main(int __atttribute__(( __unused__ )) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -29,7 +29,7 @@ int main(int __atttribute__(( __unused__ )) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) || (*op == '%' && numb2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
